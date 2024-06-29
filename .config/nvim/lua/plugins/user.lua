8b46feb6c13646492bf124b65c5c8dce784e8b63
Cozma Rares <cozmarares11@gmail.com>
2024-06-29 23:42:34 +0300

    nvim: remove cmp scrollbar, change priority of suggestions

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -129,4 +129,34 @@ return {
       on_colors = function(colors) colors.bg_float = "none" end,
     },
   },
+  { -- override nvim-cmp plugin
+    "hrsh7th/nvim-cmp",
+    -- override the options table that is used in the `require("cmp").setup()` call
+    opts = function(_, opts)
+      -- opts parameter is the default options table
+      -- the function is lazy loaded so cmp is able to be required
+      local cmp = require "cmp"
+      -- modify the sources part of the options table
+      opts.sources = cmp.config.sources {
+        { name = "nvim_lsp", priority = 1000 },
+        { name = "luasnip", priority = 750 },
+        { name = "buffer", priority = 500 },
+        { name = "path", priority = 250 },
+      }
+
+      opts.window = {
+        completion = cmp.config.window.bordered {
+          col_offset = -2,
+          side_padding = 0,
+          border = "rounded",
+          winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
+          scrollbar = false,
+        },
+        documentation = cmp.config.window.bordered {
+          border = "rounded",
+          winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
+        },
+      }
+    end,
+  },
 }
