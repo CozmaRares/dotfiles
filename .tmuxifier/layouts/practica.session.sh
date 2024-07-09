2a386dca3e0321928f77a5e19f895edbc685d8e0
Cozma Rares <cozmarares11@gmail.com>
2024-07-09 18:43:13 +0300

    tmuxifier: change session

--- .tmuxifier/layouts/practica.session.sh
+++ .tmuxifier/layouts/practica.session.sh
@@ -3,10 +3,12 @@ session_root "/mnt/SSD/repos/practica"
 if initialize_session "prac"; then
   new_window "nvim"
   run_cmd "nvim"
-  new_window "tailwind"
-  run_cmd "pnpm run dev:tailwind"
-  new_window "php"
-  run_cmd "pnpm run dev:php"
+  new_window "server"
+  run_cmd "pnpm run dev:server"
+  split_h 25
+  new_window "client"
+  run_cmd "pnpm run dev:client"
+  split_h 25
   new_window "term"
   select_window 1
 fi
